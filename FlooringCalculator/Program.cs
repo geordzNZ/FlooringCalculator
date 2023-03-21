6f2b8a36b8124fe98ab5a2f5d97f41982227cad4
GeordzNZ <geordie.king@gmail.com>
2023-03-21 22:08:43 +0000

    Task 2: Added Read user input and write to console

--- FlooringCalculator/Program.cs
+++ FlooringCalculator/Program.cs
@@ -4,10 +4,22 @@
     {
         static void Main(string[] args)
         {
-            //Header details
+            //Header section
             Console.WriteLine("\t\tWELCOME");
             Console.WriteLine("\t  Flooring Calculator");
             Console.WriteLine("=======================================\n");
+
+            //User input of section
+            Console.WriteLine("Please enter the following details to calculate the flooring costs...");
+            Console.Write("\t - Width: ");
+            string floorWidth = Console.ReadLine();
+            Console.Write("\t - Length: ");
+            string floorLength = Console.ReadLine();
+            Console.Write("\t - Unit Cost:  ");
+            string tileUnitCost = Console.ReadLine();
+
+            // Write user input 
+            Console.WriteLine($"W: {floorWidth} / L: {floorLength} / UC: {tileUnitCost}");
         }
     }
 }
