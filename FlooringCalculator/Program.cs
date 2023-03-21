8ccbb93e75826494e4444c77e13cfcd2068d2b20
GeordzNZ <geordie.king@gmail.com>
2023-03-21 21:58:03 +0000

    Task 1: Add heading info

--- FlooringCalculator/Program.cs
+++ FlooringCalculator/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Header details
+            Console.WriteLine("\t\tWELCOME");
+            Console.WriteLine("\t  Flooring Calculator");
+            Console.WriteLine("=======================================\n");
         }
     }
 }
