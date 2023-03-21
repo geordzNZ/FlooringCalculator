bd143f0a326a4d0da9e788f4260816d239b7bfb4
GeordzNZ <geordie.king@gmail.com>
2023-03-21 22:58:24 +0000

    Task 7: add calculations for cost per hour and refactor for these calcualtions

--- FlooringCalculator/Program.cs
+++ FlooringCalculator/Program.cs
@@ -11,16 +11,16 @@
 
             //User input of section
             Console.WriteLine("Enter the following details to calculate the flooring costs... \n(whole numbers only please)");
-            Console.Write("\t - Width: ");
+            Console.Write("\t - Width:\t");
             string floorWidth = Console.ReadLine();
-            Console.Write("\t - Length: ");
+            Console.Write("\t - Length:\t");
             string floorLength = Console.ReadLine();
-            Console.Write("\t - Unit Cost:  ");
+            Console.Write("\t - Unit Cost:\t");
             string tileUnitCost = Console.ReadLine();
 
             // Write user input 
-            Console.WriteLine("\n\tYou entered the following values...");
-            Console.WriteLine($"\tW: {floorWidth} / L: {floorLength} / UC: {tileUnitCost}");
+            Console.WriteLine("\nYou entered the following values...");
+            Console.WriteLine($"\tW: {floorWidth} / L: {floorLength} / UC: {tileUnitCost}\n");
 
             //Convert text to numbers (assuming correct input for now)
             int convertedFloorWidth = Int32.Parse(floorWidth);
@@ -28,8 +28,24 @@
             int convertedUnitCost = Int32.Parse(tileUnitCost);
 
             //Calculate totals
-            int totalCost = convertedFloorWidth * convertedFloorLength * convertedUnitCost;
-            Console.WriteLine($"\n\tTotal Cost for flooring is {totalCost}");
+            int roomSize = convertedFloorWidth * convertedFloorLength;
+            int flooringCost = roomSize * convertedUnitCost;
+            Console.WriteLine($"Costs breakdown...");
+            Console.WriteLine($"\tCost for flooring:\t{flooringCost}");
+
+            //Bonus stage: caclulation for non rectangular rooms
+
+
+            //Bonus stage: calculate total cost including cost per hour
+            int labourCostPerHour = 86;
+            int flooringPerHour = 20;
+            decimal workUnits = (decimal)roomSize / flooringPerHour;
+            decimal labourCost = (decimal)workUnits * labourCostPerHour;
+            
+            Console.WriteLine($"\tCost for Labour:\t{labourCost}");
+
+
+            Console.WriteLine($"\n\tTotal Cost:\t\t{flooringCost + labourCost}");
             Console.WriteLine("\n==========================================================\n");
         }
     }
